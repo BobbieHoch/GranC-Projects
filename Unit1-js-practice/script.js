df2ee06af9a19ad17f7b5a4ee53a57da99992625
BobbieHoch <rwhochstadt@gmail.com>
2022-03-17 23:39:02 -0400

    initial commit

---
+++ Unit1-js-practice/script.js
@@ -0,0 +1,41 @@
+document.write("JavaScript works!");
+
+
+console.log("Hello console!");
+
+console.warn("This is only a test!");
+
+let myName = ("Robert");
+
+let workFromHome = true;
+
+let side = 15;
+
+let radius = 10
+
+console.log(`My name is ${myName}.`);
+console.log(`I work from home: ${workFromHome}`);
+console.log(`The length of each side of the square is ${side}`);
+console.log(`the radius of the circle is ${radius}`);
+
+let squareArea = side**=2;
+
+let squarePerimeter = 4*side;
+
+let circleArea = Math.PI*radius*radius;
+
+let circlePerimeter = 2*Math.PI*radius;
+
+console.log(`The square area is ${squareArea}.`);
+console.log(`The circle area is ${circleArea} and the perimeter is ${circlePerimeter}.`);
+console.log(`The square are is${squareArea} the perimeter is ${squarePerimeter}`);
+
+let travelOptions = ["foot","bike","car","airplane" ];
+
+console.log(`The travel options are:`);
+console.log(`1) ${travelOptions[0]}`);
+console.log(`2) ${travelOptions[1]}`);
+console.log(`3) ${travelOptions[2]}`);
+console.log(`4) ${travelOptions[3]}`);
+
+
