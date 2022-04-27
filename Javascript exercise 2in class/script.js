9809bb8653ce12bbb018a3913bf59f4bf1f10710
BobbieHoch <rwhochstadt@gmail.com>
2022-04-26 23:56:11 -0400

    Commit from VSCode

---
+++ Javascript exercise 2in class/script.js
@@ -0,0 +1,12 @@
+const actualTemp = 72;
+const desiredTemp = 68;
+
+if (actualTemp > desiredTemp){
+    console.log('Run A/C');
+}
+else if (actualTemp < desireTemp){
+    console.log('Run Heat');
+} 
+else{
+    console.log('Standby');
+}
