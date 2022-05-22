2fd84b8575a71a027cdde4b1693bacd7eaec7f8a
BobbieHoch <rwhochstadt@gmail.com>
2022-05-22 18:22:21 -0400

    Can't get last two to print out

--- 2-Jscript-folders/2nd try-Jscript-assessment-2-BobbieHoch/script.js
+++ 2-Jscript-folders/2nd try-Jscript-assessment-2-BobbieHoch/script.js
@@ -1,31 +1,72 @@
-const weekDays = ["Monday", "Wednesday", "Friday"];
+// const weekDays = ["Monday", "Wednesday", "Friday"];
 
-const rectangle = { height: 20, width: 15 };
+// const rectangle = { height: 20, width: 15 };
 
-const greetMe = function (name) {
-  console.log(`Hi ${name}!`);
-};
-greetMe("Fred");
+// const greetMe = function (name) {
+//   console.log(`Hi ${name}!`);
+// };
+// greetMe("Fred");
 
-const calculateTriangleArea = function (base, height) {
-  console.log((base * height) / 2);
-  return (base * height) / 2;
-};
+// const calculateTriangleArea = function (base, height) {
+//   console.log((base * height) / 2);
+//   return (base * height) / 2;
+// };
 
-calculateTriangleArea(2, 4);
+// calculateTriangleArea(2, 4);
 
-function countDown() {
-  let i = 10;
-  while (i >= 1) {
-    console.log(i);
-    i--;
-  }
+// function countDown() {
+//   let i = 10;
+//   while (i >= 1) {
+//     console.log(i);
+//     i--;
+//   }
+// }
+// countDown();
+
+// let i = 2;
+// const countEvens = function (end) {
+//   while (i <= end && i % 2 === 0) {
+//     console.log(i);
+//     i++;
+//   }
+// };
+// countEvens(17);
+
+// function checkAdverb(word) {
+//   let i = word.length;
+//   if (word.indexof("l") === i && word.indexof("y") === i + 1) {
+//     console.log("adverb");
+//     return "adverb";
+//   } else {
+//     console.log("unknown");
+//     return "unknown";
+//   }
+// }
+// checkAdverb(quickly);
+// checkAdverb(running);
+// 8.
+// const materialsList=[
+//   {type:'straw', strength: 1 },
+//   {type:'wood', strength: 5 },
+//   {type:'brick', strength: 10 }
+// ]
+
+const materialsList = [
+  { type: "straw", strength: 1 },
+  { type: "wood", strength: 5 },
+  { type: "brick", strength: 10 },
+];
+
+function printMaterials(materialsList) {
+  return `${type}+ "-" + ${strength}`;
 }
-countDown();
+console.log(printMaterials);
 
-const countEvens(end)={
-  let i = 2;
-  while (i >= end && end % 2===0)
-  console.log(i);
-  i++
+
+const getStrengthOfMaterial = function (materialsList, type){
+const selectType = (type === selectType && strength > 0){
+  console.log(`The strength of  ${type} is ${strength}`;)
+}
+} 
+  return 
 }
